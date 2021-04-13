50ac41a3133c39aabefb5040520b4519e966e4cf
thaiph99 <thaiph99@gmail.com>
2021-04-13 17:24:41 +0700

    update code test face-recognition

--- face-recognition-test/multi_face_svm.py
+++ face-recognition-test/multi_face_svm.py
@@ -31,6 +31,9 @@ import face_recognition
 from sklearn import svm
 from os import getcwd
 import os
+import json
+import numpy as np
+from json import JSONEncoder
 import matplotlib.pyplot as plt
 
 # Training the SVC classifier
@@ -49,13 +52,13 @@ print(train_dir)
 print('encoding face image')
 
 for person in train_dir:
-    pix = os.listdir(getcwd()+"/train_dir/" + person)
+    pix = os.listdir(getcwd() + "/train_dir/" + person)
 
     # Loop through each training image for the current person
     for person_img in pix:
         # Get the face encodings for the face in each image file
         face = face_recognition.load_image_file(
-            getcwd()+"/train_dir/" + person + "/" + person_img)
+            getcwd() + "/train_dir/" + person + "/" + person_img)
         face_bounding_boxes = face_recognition.face_locations(face)
 
         # If training image contains exactly one face
@@ -68,18 +71,50 @@ for person in train_dir:
             print(person + "/" + person_img +
                   " was skipped and can't be used for training")
 
-print(encodings[0])
-print(encodings[0].shape)
 # print(encodings[0])
+# create dict to save to json
+json_file = {}
+for name in names:
+    json_file[name] = []
+for i in range(len(encodings)):
+    json_file[names[i]].append(encodings[i])
+
+
+# print(json_file)
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
+
+
+# print(json_file)
+
+with open('face_data.json', 'w') as f:
+    json.dump(json_file, f, cls=NumpyArrayEncoder)
+
+with open('face_data.json', 'r') as f:
+    data = json.load(f)
+print(type(data))
+
+vec_face = []
+list_name = []
+for name in data.keys():
+    for fa in data[name]:
+        vec_face.append(fa)
+        list_name.append(name)
+
+print(list_name)
+print(len(vec_face))
 
 # Create and train the SVC classifier
 clf = svm.SVC(gamma='scale')
 print('training face')
-clf.fit(encodings, names)
+clf.fit(vec_face, list_name)
 print('done')
 
 # Load the test image with unknown faces into a numpy array
-test_image = face_recognition.load_image_file('img3.jpg')
+test_image = face_recognition.load_image_file('img2.jpg')
 
 # Find all the faces in the test image using the default HOG-based model
 face_locations = face_recognition.face_locations(test_image)
