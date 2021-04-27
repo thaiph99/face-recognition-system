b6cdefdba6df9078d2bfc8967239355ac3fc587c
thaiph99 <thaiph99@gmail.com>
2021-04-27 12:05:46 +0700

    update face detect

--- camera.py
+++ camera.py
@@ -3,17 +3,25 @@ __author__ = 'thaiph99'
 from imutils.video import WebcamVideoStream
 import cv2
 import face_recognition
+import dlib
+import numpy as np
+
+
+# predictor_path = 'testcode/shape_predictor_5_face_landmarks.dat'
 
 
 class VideoCamera(object):
     def __init__(self):
         self.stream = WebcamVideoStream(src=0).start()
+        self.detertor = dlib.get_frontal_face_detector()
+        self.predictor = dlib.shape_predictor(
+            'testcode/shape_predictor_5_face_landmarks.dat')
         # with open()
 
     def __del__(self):
         self.stream.stop()
 
-    def get_frame(self):
+    def get_frame_by_face_recognition(self):
         img = self.stream.read()
         img = cv2.flip(img, 1)
 
@@ -28,5 +36,43 @@ class VideoCamera(object):
                           (endX, endY), (0, 255, 0), 2)
 
         _, jpg = cv2.imencode('.jpg', img)
+        jpg = jpg.tobytes()
+        return jpg
+
+    @staticmethod
+    def shape_to_np(shape, dtype="int"):
+        # print('type shape :', type(shape))
+        coords = np.zeros((5, 2), dtype=dtype)
+        for i in range(0, 5):
+            coords[i] = (shape.part(i).x, shape.part(i).y)
+        return coords
+
+    def get_frame(self):
+        frame = self.stream.read()
+        frame = cv2.flip(frame, 1)
+        frame_new = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        dets = self.detertor(frame_new, 1)
+        for i, det in enumerate(dets):
+            startX = int(det.left())
+            startY = int(det.top())
+            endX = int(det.right())
+            endY = int(det.bottom())
+            cv2.rectangle(frame, (startX, startY),
+                          (endX, endY), (0, 255, 0), 2)
+
+        faces = dlib.full_object_detections()
+        faces = self.predictor(frame_new, dets[0])
+
+        points = self.shape_to_np(faces)
+
+        for i in points:
+            x = i[0]
+            y = i[1]
+            cv2.circle(frame, (x, y), 2, color=(0, 0, 0), thickness=2)
+
+        image = dlib.get_face_chip(frame_new, faces, size=320)
+        cv_bgr_img = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        cv2.imwrite('datatmp/img_in_sec.jpg', cv_bgr_img)
+        _, jpg = cv2.imencode('.jpg', frame)
         jpg = jpg.tobytes()
         return jpg
--- testcode/code_facedetect.py
+++ testcode/code_facedetect.py
@@ -1,3 +1,4 @@
+
 # coding:utf-8
 '''
 Face position calibration
@@ -5,13 +6,14 @@ Face position calibration
 import cv2
 import dlib
 import numpy as np
+import time
 
 
 def shape_to_np(shape, dtype="int"):
     print('type shape :', type(shape))
-    coords = np.zeros((68, 2), dtype=dtype)
+    coords = np.zeros((5, 2), dtype=dtype)
     for i in range(0, 5):
-        coords[i] = (shape.parts(i).x, shape.parts(i).y)
+        coords[i] = (shape.part(i).x, shape.part(i).y)
     return coords
 
 
@@ -24,17 +26,28 @@ if not camera.isOpened():
     print("cannot open camear")
     exit(0)
 
+start = time.time()
+
 while True:
     ret, frame = camera.read()
-
+    print('ret : ', ret)
     if not ret:
         continue
     frame = cv2.flip(frame, 1)
-    cv2.imshow('camera', frame)
     frame_new = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     # Detect face
     dets = detector(frame_new, 1)
+    for i, det in enumerate(dets):
+        left = det.left()
+        right = det.right()
+        top = det.top()
+        bottom = det.bottom()
+        cv2.rectangle(frame, (left, top), (right, bottom),
+                      color=(0, 0, 0), thickness=5)
+
+    cv2.imshow('camera', frame)
+
     print('dets : ', type(dets))
     # cv2.rectangle(frame_new, dets[0][0], dets[0][1], color=(0, 0, 0))
     print("Number of faces detected: {}".format(len(dets)))
@@ -47,23 +60,23 @@ while True:
     # for detection in dets:
     faces = sp(frame_new, dets[0])
     print('type : ', type(faces))
-    print(faces)
-    # points = []
-    # for face in faces:
-    #     points.append(shape_to_np(faces))
     points = shape_to_np(faces)
+    print('points : ', points)
+    print('len point :', len(points))
     for i in points:
         x = i[0]
         y = i[1]
-        cv2.circle(image, (x, y), 2, (0, 255, 0), -1)
-
+        cv2.circle(frame, (x, y), 2, color=(0, 0, 0), thickness=-1)
+    cv2.imshow('camera', frame)
     image = dlib.get_face_chip(frame_new, faces, size=320)
-    # for image in images:
     cv_bgr_img = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    cv2.imshow('image', cv_bgr_img)
-    # image = dlib.get_face_chip(frame_new, faces[0])
-    # cv_bgr_img = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    # cv2.imshow('image', cv_bgr_img)
+    # cv2.imshow('face', cv_bgr_img)
+    end = time.time()
+    cv2.imwrite('data_save/img_in_sec'+str(end-start)+'.jpg', cv_bgr_img)
+
+    print('time : ', 300 - int(end-start))
+    if end-start >= 60*3:
+        break
 
     key = cv2.waitKey(1)
     if key == 27:
