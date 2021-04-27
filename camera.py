25d3003c13f21d3c20c9bd319e790dd1d6e813ea
thaiph99 <thaiph99@gmail.com>
2021-04-27 17:33:03 +0700

    update detect pose

--- camera.py
+++ camera.py
@@ -70,6 +70,9 @@ class VideoCamera(object):
                 x = i[0]
                 y = i[1]
                 cv2.circle(frame, (x, y), 2, color=(0, 0, 0), thickness=2)
+
+            
+
             image = dlib.get_face_chip(frame_new, faces, size=320)
             cv_bgr_img = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
             cv2.imwrite('datatmp/img_in_sec.jpg', cv_bgr_img)
